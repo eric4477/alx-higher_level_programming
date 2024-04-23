66233983067fb3f8b4ba7fd8e4f7ff09878c43d7
eric <ericgeorge4477@gmail.com>
2024-04-24 01:31:43 +0200

    fixed format

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const fs = require("fs");
+const fs = require('fs');
 
-fs.readFile(process.argv[2], "utf8", (err, data) => {
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 
 request(process.argv[2], (err, res) => {
   if (err) {
     console.error(err);
+    return;
   }
-  console.log("code:", res.statusCode);
+  console.log('code:', res.statusCode);
 });
